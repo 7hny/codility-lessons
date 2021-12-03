0ac993fd22e08f3a3b3a67b13f810e7088e51613
7hny <baruog@gmail.com>
2021-12-03 22:25:20 +0100

    Update FrogRiverOne.js

--- lesson-04/FrogRiverOne.js
+++ lesson-04/FrogRiverOne.js
@@ -4,23 +4,35 @@
 // 72%
 // https://app.codility.com/demo/results/trainingSJSED2-8ZP/
 
+// 100% as per https://gist.github.com/matheushf/d1237d8d78865b328d63a96f6c632c36
+// https://app.codility.com/demo/results/trainingEG9FXJ-EME/
+
 function solution(X, A) {
-    // write your code in JavaScript (Node.js 8.9.4)
-    let occurences = firstOccurence(A, X);
-    let lastLeaf = -1;
+    // write your code in JavaScript (Node.js 6.4.0) 
 
-    for(let i = 1; i<occurences.length; i++){
-        if(occurences[i]===0 && X > 1) return -1;
-        if(lastLeaf < occurences[i] ) lastLeaf = occurences[i];
+    let sequence = [0];
+    let seconds = -1;
+    let counter = 0;
+
+    if (X === 1 && A[0] === 1)
+        return 0;
+
+    for (let i = 0; i <= A.length - 1; i++) {
+
+        if (A[i] <= X) {
+            if (!sequence[A[i]]) {
+                // we count each unique jump
+                counter++;
+            }
+            // we flag a jump taht we counted
+            sequence[A[i]] = 1;
+
+            if (counter === X) {
+                // solution is a loop iteration index when last jump was counted
+                seconds = i;
+                break;
+            }
+        }
     }
-
-    return lastLeaf;
-}
-
-function firstOccurence(A, n){
-    let occurences = new Array(n+1).fill(0);
-    for(i=A.length-1; i>0; i--){
-        occurences[A[i]]=i;
-    }
-    return occurences;
+    return seconds;
 }
