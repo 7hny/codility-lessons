ea3b087dd5f117346a40112c108ec549fd84277e
7hny <baruog@gmail.com>
2021-12-03 19:38:02 +0100

    Create FrogRiverOne.js

---
+++ lesson-04/FrogRiverOne.js
@@ -0,0 +1,23 @@
+// 81%
+// https://app.codility.com/demo/results/trainingJP7XFZ-KXT/
+
+function solution(X, A) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    let occurences = firstOccurence(A, X);
+    let lastLeaf = -1;
+
+    for(let i = 1; i<occurences.length; i++){
+        if(occurences[i]===0 && X > 1) return -1;
+        if(lastLeaf < occurences[i] ) lastLeaf = occurences[i];
+    }
+
+    return lastLeaf;
+}
+
+function firstOccurence(A, n){
+    let occurences = new Array(n+1).fill(0);
+    for(i=A.length-1; i>0; i--){
+        occurences[A[i]]=i;
+    }
+    return occurences;
+}
