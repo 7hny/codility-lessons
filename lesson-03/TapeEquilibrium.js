8372eb43b758b56d1448204998195d38e58b0bea
7hny <baruog@gmail.com>
2021-12-03 19:33:58 +0100

    Create TapeEquilibrium.js

---
+++ lesson-03/TapeEquilibrium.js
@@ -0,0 +1,23 @@
+function solution(A) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    const arr = A;
+
+    let leftSum = 0;
+    let rightSum = 0;
+    let smallest = Number.MAX_SAFE_INTEGER;
+    
+    for(i = 0; i<arr.length; i++){
+        rightSum += arr[i];
+    }
+
+    for(i = 0; i<arr.length-1; i++){
+
+        leftSum += arr[i];
+        rightSum -= arr[i];
+
+        let sum = Math.abs(leftSum - rightSum);
+        if(sum<smallest) smallest = sum;
+    }
+
+    return smallest;
+}
