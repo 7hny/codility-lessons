9e5fd23c82f4d66d517d0397170cc167e16bc116
7hny <baruog@gmail.com>
2021-12-03 23:20:12 +0100

    Update PermCheck.js

--- lesson-04/PermCheck.js
+++ lesson-04/PermCheck.js
@@ -16,3 +16,24 @@ function solution(A) {
         return 0;
     }
 }
+
+// 58%
+// https://app.codility.com/demo/results/trainingDW7G7J-NPT/
+
+function solution(A) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    let arr = A;
+
+    arr.sort((a,b)=> {
+        return a-b
+    })
+
+    let sum = arr.reduce((acc, a)=>{
+        return acc+a;
+    }, 0)
+
+    let step = arr[1] - arr[0];
+    let calculatedSum = arr.length*(arr.length+step)/2
+
+    return (sum === calculatedSum) ? 1 : 0;
+}
