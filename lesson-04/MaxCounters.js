085533bf63083c7bf6eab055775b4c79c733a372
7hny <baruog@gmail.com>
2021-12-05 19:40:31 +0100

    Update MaxCounters.js

--- lesson-04/MaxCounters.js
+++ lesson-04/MaxCounters.js
@@ -1,34 +1,37 @@
-// 77%
-// https://app.codility.com/demo/results/trainingG28SM3-CPW/
+// 100%
+// https://app.codility.com/demo/results/trainingR42NKD-GVN/
 
 function solution(N, A) {
     // write your code in JavaScript (Node.js 8.9.4)
 
     let arr = A;
-
     let counter = new Array(N+1).fill(0);
-
-    //console.log(counter)
-
     let lastMax = 0;
+    let max = 0;
 
     for(let i=0; i<arr.length; i++){
 
         let item = arr[i];
-        //let prevItem = arr[i-1];
 
         if(item <= N){
+            // update currenly processed couter to lst max couter value
+            if(counter[item] < max) counter[item] = max;
             counter[item] += 1;
+            // remember last maximum couter value
             if(counter[item] > lastMax) lastMax = counter[item];
         }
 
         if(item == N+1){
-            counter.fill(lastMax);
+            max = lastMax;
         } 
     }
 
     counter.shift();
-    // console.log(counter)
+
+    for(let i=0; i<counter.length; i++){
+        // fill remaining zero value counters
+        if(counter[i] < max) counter[i] = max;
+    }
 
     return counter;
 }
