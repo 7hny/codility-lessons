ab9beb23f2bac2da124dd1a7c4fae913f8cde106
7hny <baruog@gmail.com>
2021-12-06 01:09:03 +0100

    Update CountDiv.js

--- lesson-05/CountDiv.js
+++ lesson-05/CountDiv.js
@@ -1,13 +1,18 @@
-// 60%
-// https://app.codility.com/demo/results/trainingFDPVRX-4M2/
+// 62%
+// https://app.codility.com/demo/results/trainingCQJ2G7-ZC5/
 
 function solution(A, B, K) {
     // write your code in JavaScript (Node.js 8.9.4)
     let start = A+A%K;
     let end = B-B%K;
 
+    if(K>B) return 0;
+    if(K==B) return 1;
+
     let result = (end - start)/K+1
 
+    // console.log(start)
+    // console.log(end) 
     //console.log(result)
-    return result;
+    return Math.trunc(result);
 }
