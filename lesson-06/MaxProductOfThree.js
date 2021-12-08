743d8249d563ca3f2e02b9c0490f924f6fb1b667
7hny <baruog@gmail.com>
2021-12-08 19:49:21 +0100

    Create MaxProductOfThree.js

---
+++ lesson-06/MaxProductOfThree.js
@@ -0,0 +1,12 @@
+// 22%
+// https://app.codility.com/demo/results/trainingHJFE23-NT2/
+
+function solution(A) {
+    let arr = A;
+    arr.sort();
+
+    let result = arr[arr.length-3] * arr[arr.length-2] * arr[arr.length-1];
+    return result;
+}
+
+// [-5, 5, -5, 4]
