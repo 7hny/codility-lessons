9aee8bd1623d2a7d83dcb8a9e2a9e257ff26cf25
7hny <baruog@gmail.com>
2021-12-08 18:50:57 +0100

    Update MinAvgTwoSlice.js

--- lesson-05/MinAvgTwoSlice.js
+++ lesson-05/MinAvgTwoSlice.js
@@ -1,5 +1,5 @@
-// 80%
-// https://app.codility.com/demo/results/trainingMXZ32F-S9X/
+// 90%
+// https://app.codility.com/demo/results/trainingW8PMY7-GR2/
 
 function solution(A) {
     // write your code in JavaScript (Node.js 8.9.4)
@@ -8,6 +8,8 @@ function solution(A) {
     let minAvg = null;
     let pos = arr.length;
 
+    // we are looking for 2 or 3 element slices
+    // this objective is not given in task description
     for(let i = 0; i < arr.length-2; i++){
         let sum1 = arr[i] + arr[i+1];
         let sum2 = sum1 + arr[i+2];
@@ -19,6 +21,13 @@ function solution(A) {
             minAvg = avg;
             pos = i;
         }
+    }
+
+    // we nned to include value of a slide
+    // that is omited in main loop iteration
+    let avg = (arr[arr.length - 2] + arr[arr.length - 1])/2;
+    if(avg < minAvg){
+        pos = arr.length - 2;
     }
 
     return pos;
