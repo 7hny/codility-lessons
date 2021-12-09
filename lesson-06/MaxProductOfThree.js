b1c86d40cba5c11ffc745fdcb1cb2dff02a175dd
7hny <baruog@gmail.com>
2021-12-09 18:47:28 +0100

    Update MaxProductOfThree.js

--- lesson-06/MaxProductOfThree.js
+++ lesson-06/MaxProductOfThree.js
@@ -1,13 +1,17 @@
-// 22%
-// https://app.codility.com/demo/results/trainingHJFE23-NT2/
+// 44%
+// https://app.codility.com/demo/results/training8KE792-6M9/
 
 function solution(A) {
     let arr = A;
     arr.sort();
+    //A.sort((a, b) => (a - b));
 
-    let result = arr[arr.length-3] * arr[arr.length-2] * arr[arr.length-1];
-    return result;
+    // console.log(arr)
+
+    let result1 = arr[arr.length-3] * arr[arr.length-2] * arr[arr.length-1];
+    let result2 = arr[0] * arr[1] * arr [arr.length-1];
+    return Math.max(result1, result2);
 }
 
 // [-5, 5, -5, 4] 125
-
+//  [-5, -6, -4, -7, -10] -120
