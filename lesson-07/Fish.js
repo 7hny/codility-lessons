769775dd4402ae0ace35a36071b8528abba8652f
7hny <baruog@gmail.com>
2021-12-12 14:34:36 +0100

    Update Fish.js

--- lesson-07/Fish.js
+++ lesson-07/Fish.js
@@ -1,28 +1,32 @@
-// 37%
-// https://app.codility.com/demo/results/trainingKA2RM9-NMT/
+// 100%
+// https://app.codility.com/demo/results/training4VMDKW-GZY/
 
 function solution(A, B) {
     // write your code in JavaScript (Node.js 8.9.4)
 
     let stack = [];
     let n = A.length;
-    let survivals = 0;
+    let eaten = 0;
 
     for(i=0; i<n; i++){
         if(B[i] == 0){
             // downstream
-            if(stack.length>0){
-                if(stack[stack.lenght-1]<A[i]){
+            while(stack.length>0){
+                // we count eaten fish
+                if(stack[stack.length-1]<A[i]){
                     stack.pop();
+                    eaten ++;
+                } else if(stack[stack.length-1]>A[i]){
+                    eaten ++;
+                    break;
                 }
-            } else {
-                survivals++;
             }
         } else {
             // upstream
             stack.push(A[i]);
         }
     }
+    // we subtract eaten fish from total number of fish
+    return n - eaten;
 
-    return survivals+stack.length;
 }
