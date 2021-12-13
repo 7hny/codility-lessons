ff0d3eb685c938f355518f4f8489efe1505c9ed4
7hny <baruog@gmail.com>
2021-12-13 20:26:00 +0100

    Update MaxSliceSum.js

--- lesson-09/MaxSliceSum.js
+++ lesson-09/MaxSliceSum.js
@@ -1,17 +1,19 @@
-// 53%
-// https://app.codility.com/demo/results/training5GUSXS-K2G/
+// 100%
+// https://app.codility.com/demo/results/training36XW7A-HUY/
 
 function solution(A) {
-    // write your code in JavaScript (Node.js 8.9.4)
     let n = A.length;
     let sum = 0;
-    let maxSlice = 0;
+    // important! init with first element value af initial max slice value
+    // this will allow ta take small sets with megative max slice value into account
+    let maxSlice = A[0];
 
     // Kadane's algorithm:
-    // when sum becomes negative
-    // it's best to consider starting a new slice from current position
+    // when sum at current position is lesset than current position value
+    // it's best to consider starting a new slice using current position value
     for(let i=0; i<n; i++){
-        sum = Math.max(0, (sum+A[i]));
+        sum += A[i];
+        if(A[i]>sum) sum = A[i];
         maxSlice = Math.max(maxSlice, sum);
     }
 
