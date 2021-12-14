793e1fbbd29d395bbbd3d1f3e7f1f471afaef6dc
7hny <baruog@gmail.com>
2021-12-14 19:31:48 +0100

    Update Flags.js

--- lesson-10/Flags.js
+++ lesson-10/Flags.js
@@ -1,10 +1,12 @@
-// 33%
-// https://app.codility.com/demo/results/training9535JZ-5G7/
+// 73%
+// https://app.codility.com/demo/results/training953F79-MG3/
 
 function solution(A) {
     // write your code in JavaScript (Node.js 8.9.4)
     let n = A.length;
+    if(n<3) return 0;
     let peaks = [];
+
 
     for(i=1; i<n-1; i++){
         if(A[i-1] < A[i] && A[i+1] < A[i]){
@@ -12,20 +14,32 @@ function solution(A) {
         }
     }
 
-    // console.log(peaks);
+    let nP = peaks.length;
 
-    let lastFlag = null;
-    let flags = 0;
-    for(i=0; i<peaks.length; i++){
-        if(lastFlag == null){
-            lastFlag = peaks[i];
-            flags++;
+    if(nP == 0) return 0;
+    if(nP == 1) return 1;
+
+    let flags = 1;
+    let maxFlags = 0;
+
+    // k = we check every possible number of flags but no more than peaks
+    // or sqrt(n), whichever smaller
+    for(let k = Math.min(nP, parseInt(Math.sqrt(n)))+1; k>=1; k--){
+        let lastFlag = 0;
+        flags = 1;
+        // check the distance between flags
+        // and count them to see if we can fit them onto available peaks
+        for(i=0; i<peaks.length; i++){
+            if(peaks[i] - lastFlag >= k && flags < k){
+                lastFlag = peaks[i]
+                flags++;
+            }
         }
-        else if(peaks[i] - lastFlag >= peaks.length){
-            lastFlag = peaks[i]
-            flags++;
-        }
+        // return max num of flags if we found it
+        if(flags<maxFlags) return maxFlags;
+        // if not store it to compare against further iterations
+        else if(maxFlags<flags) maxFlags = flags;
     }
 
-    return flags
+    return maxFlags;
 }
